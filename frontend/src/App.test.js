253d4e791cc9966e5664bb94b70127f40e056c9f
Yash Chauhan <yashchauhan125dpr@gmail.com>
2026-02-12 12:26:12 +0530

    Delete frontend/src/App.test.js

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app heading', () => {
-  render(<App />);
-  const text = screen.getByText(/task management system/i);
-  expect(text).toBeInTheDocument();
-});
